f6125ac13f512c8c204f306a6c0a7d476e4b6ccb
Dinesh Dixit(Mechatron) <114578318+Mechatron00@users.noreply.github.com>
2023-09-10 20:53:37 +0530

    modal-added

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -26,6 +26,35 @@ export const templesData = [
 
             "architecture": "Architecture 1"
         }
+    },
+    {
+        "basic": {
+            "name": "Brihadisvara",
+            "location": {
+                "pin": "Bhatkal",
+                "dist": "Thanjavur",
+                "state": "Tamil Nadu"
+            },
+            "coordinates": {
+                "x":10.782778,
+                "y":79.131667
+            }
+        },
+        "Architecture": {
+            "style": "Style 1",
+            "creator": "Creator 1",
+            "completed": "Completion Date 1",
+            "inscriptions": "Inscriptions 1",
+            "elevation": 100
+        },
+        "about": {
+            "basic_info": "Brihadishvara Temple, called Rajarajesvaram ('Lord of Rajaraja') by its builder, and known locally as Thanjai Periya Kovil ('Thanjavur Big Temple') and Peruvudaiyar Kovil, is a Shaivite Hindu temple built in a Chola architectural style located on the south bank of the Cauvery river in Thanjavur, Tamil Nadu, India. ",
+
+            "history": "History 1",
+
+            "architecture": "Architecture 1"
+        }
     }
+   
    
 ]
