26db76c54ac35428fe78f34c697d408e36788841
Dinesh Dixit(Mechatron) <114578318+Mechatron00@users.noreply.github.com>
2023-09-17 18:06:00 +0530

    styles added

--- src/app/data/mantras.ts
+++ src/app/data/mantras.ts
@@ -9,7 +9,12 @@ export const mantras =[
         name:"शिव तांडव ",
         src:"../../assets/pdfs/shiva_tandav.pdf",
         cover:"../../assets/covers/shivatandav.png"
-    }
+    },
+    {
+        name:"शिव पंचाक्षर स्तोत्र ",
+        src:"../../assets/pdfs/shiv_panchakshar.pdf",
+        cover:"../../assets/covers/panchakshari.png"
+    },
 ]
 
 
